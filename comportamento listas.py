657d86ef06b322e45b00efbf3820251f0b25bb8a
Glayde Regina Toledo <grtoledo98@gmail.com>
2024-09-30 11:39:23 -0300

    aula segunda 1

---
+++ comportamento listas.py
@@ -0,0 +1,17 @@
+# Demostração de comportamento de listas...
+print("Vou almoçar em um restaurante a quilo!")
+
+Original = ["arroz", "feijão", "batata", "alface", "frango"]
+print("eis, a minha comida:0, Original")
+Derivada = Original
+print("Meu amigo irá comer também:", Derivada)
+
+print("Vou alterar as opções sem ele ver...")
+Original.remove("arroz")
+Original.remove("feijão")
+Original.remove("alface")
+Original.append("picanha")
+Original.append("linguiça")
+
+print("Amiguinho, me mostre o que você vai comer?")
+print("Claro! Dá uma conferida", Derivada)
